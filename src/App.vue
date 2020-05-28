8e040a9de509415cbeef48c48dcc54962dc8b763
manav bhardwaj <mbhardwaj8@sapient.com>
2020-05-28 15:38:06 +0530

    changing port and setting up App Vue files.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<main id="app" class="d-flex flex-column flex-sm-column flex-md-row py-5 col col-12 col-sm-12 bg">
+		<h1 class="center">Rick and Morty Vue App</h1>
+	</main>
+</template>
+
+<script>
+export default {
+	name: "app",
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
